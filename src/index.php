9bbcc65b48d20d7558c1463b363348b51fa6b45e
Ed Khalturin <dracveg@gmail.com>
2022-09-13 11:05:10 +0300

    init peresda4a

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
@@ -10,26 +11,49 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 class Entrypoint {
     function main(): void {
-        $env = Dotenv::createArrayBacked(dirname(__DIR__))->load();
-        $app = AppFactory::create();
+        Dotenv::createImmutable(dirname(__DIR__))->load();
         $pdo = new PDO(
-                dsn: "mysql:host={$env['DB_HOST']};port={$env['DB_PORT']};dbname={$env['DB_NAME']}",
-                username: $env['DB_USERNAME'],
-                password: $env['DB_PASSWORD'],
+                dsn: "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",
+                username: $_ENV['DB_USERNAME'],
+                password: $_ENV['DB_PASSWORD'],
                 options: array(
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 )
         );
 
-        $app->any('/ping', function(Request $request, Response $response) {
-            $this->m();
-            $response->getBody()->write('meow');
-            return $response;
+        // Server
+
+        $app = AppFactory::create();
+        $app->add(array($this, 'authenticate'));
+
+        $app->any('/ping', function(Request $req, Response $r) {
+            return $this->response($r, [
+                    'get' => $this->getGetParams($req),
+                    'post' => $this->getPostParams($req),
+            ]);
+        });
+
+        $app->post('/admin/initDatabase', function(Request $req, Response $r) {
+            return $this->response($r, ['status' => 'admin']);
         });
 
         $app->run();
     }
 
-    function m(){}
+    function response(Response $r, $body = array(), $code = 200): Response {
+        $r->getBody()->write(json_encode($body));
+        $r = $r->withHeader('Content-Type', 'application/json');
+        $r = $r->withStatus($code);
+        return $r;
+    }
+
+    function getPostParams(Request $req): ?array {
+        return json_decode($req->getBody(), true);
+    }
+
+    function getGetParams(Request $req): ?array {
+        return $req->getQueryParams();
+    }
+
 }
