aea1e802c2d41e796177665efb62ddafe35e7c99
Ed Khalturin <dracveg@gmail.com>
2022-09-13 17:05:33 +0300

    F подгрузка данных на чтении

--- frontend/src/features/PageList.vue
+++ frontend/src/features/PageList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="pageList">
+    <el-checkbox v-model="showRawId" label="Показывать ID" />
     <div class="table">
       <el-table :data="tableData">
         <el-table-column
@@ -18,24 +19,50 @@ import {computed, ref, watch} from "vue";
 import {useRoute} from "vue-router"
 import {useStore} from "@/features/useStore.js";
 
-const entities = ref([])
-const table = ref(null)
-
 const route = useRoute()
 const store = useStore()
-watch([route, store.data, store.tables], ([route]) => {
-  entities.value = store.data.value[route.query['t']]
-  table.value = store.tables.value[route.query['t']]
-}, {immediate: true})
 
-const tableData = computed(() => {
-  return entities.value
-})
+const table = ref(null)
+const entities = ref([])
+
+watch([route, store.data, store.tables], ([route]) => {
+  table.value = store.tables.value[route.query['t']]
+  entities.value = store.data.value[route.query['t']]
+}, {immediate: true})
 
 const columns = computed(() => {
   if (!table.value) return []
   return Object.keys(table.value)
 })
+
+const showRawId = ref(false)
+const tableData = computed(() => {
+  if (!entities.value) return []
+  return entities.value.map(entity => {
+    const result = {}
+    for (const key in entity) {
+      result[key] = entity[key]
+
+      if (key?.startsWith('id_') && !showRawId.value) {
+        const t = key.slice(3)
+        let replacer = '(?)';
+        if (t === 'mineral') {
+          replacer = store.data.value['mineral'].find(e => e.id === entity[key])?.name ?? '(?)'
+        }
+        if (t === 'shaft') {
+          replacer = store.data.value['shaft'].find(e => e.id === entity[key])?.name ?? '(?)'
+        }
+        if (t === 'supplier') {
+          replacer = store.data.value['supplier'].find(e => e.id === entity[key])?.name ?? '(?)'
+        }
+        result[key] = replacer
+      }
+    }
+    return result
+  })
+})
+
+
 
 </script>
 
