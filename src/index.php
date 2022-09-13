efe899eab0d2838012a1c5262d71a8cd917fe39e
Ed Khalturin <dracveg@gmail.com>
2022-09-13 12:06:53 +0300

    Создал структуру БД

--- src/index.php
+++ src/index.php
@@ -9,7 +9,69 @@ use Slim\Psr7\Response;
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 (new Entrypoint())->main();
 
+
 class Entrypoint {
+
+    public const SQL_INIT = <<<SQL
+create table if not exists mineral
+(
+    id   int          not null auto_increment,
+    name varchar(255) not null,
+
+    primary key (id)
+);
+
+
+create table if not exists supplier
+(
+    id      int          not null auto_increment,
+    name    varchar(255) not null,
+    country varchar(3)   not null default 'RUS',
+    city    varchar(255),
+
+    primary key (id)
+);
+
+
+create table if not exists shaft
+(
+    id          int          not null auto_increment,
+    name        varchar(255) not null,
+    id_mineral  int          not null,
+    city        varchar(255),
+    id_supplier int          not null,
+
+    primary key (id),
+    foreign key (id_mineral) references mineral (id),
+    foreign key (id_supplier) references supplier (id)
+);
+
+
+create table if not exists report
+(
+    id       int   not null auto_increment,
+    id_shaft int   not null,
+    year     int   not null,
+    amount   float not null,
+
+    primary key (id),
+    foreign key (id_shaft) references shaft (id)
+);
+
+
+create table if not exists shipment
+(
+    id          int  not null auto_increment,
+    id_supplier int  not null,
+    id_mineral  int  not null,
+    date        date not null default(CURRENT_DATE),
+
+    primary key (id),
+    foreign key (id_supplier) references supplier (id),
+    foreign key (id_mineral) references mineral (id)
+);
+SQL;
+
     function main(): void {
         Dotenv::createImmutable(dirname(__DIR__))->load();
         $pdo = new PDO(
@@ -25,7 +87,6 @@ class Entrypoint {
         // Server
 
         $app = AppFactory::create();
-        $app->add(array($this, 'authenticate'));
 
         $app->any('/ping', function(Request $req, Response $r) {
             return $this->response($r, [
@@ -34,8 +95,11 @@ class Entrypoint {
             ]);
         });
 
-        $app->post('/admin/initDatabase', function(Request $req, Response $r) {
-            return $this->response($r, ['status' => 'admin']);
+        $app->post('/initdb', function(Request $req, Response $r) use ($pdo) {
+            $params = $this->getPostParams($req);
+            if ($params['p'] !== $_ENV['ADMIN_PASSWORD']) return $this->response($r, [], 403);
+            $pdo->exec(self::SQL_INIT);
+            return $this->response($r, [], 200);
         });
 
         $app->run();
