e479b73e1160cbb55a473001af5f7901b78139c7
Ed Khalturin <dracveg@gmail.com>
2022-09-13 16:36:49 +0300

    F отображение данных база

--- frontend/src/features/PageList.vue
+++ frontend/src/features/PageList.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="pageList">
-    pageList
+    {{table}}
+    <br />
+    {{entities}}
   </div>
 </template>
 
-<script>
+<script setup>
+import {ref, watch} from "vue";
+import {useRoute} from "vue-router"
+import {useStore} from "@/features/useStore.js";
+
+const entities = ref([])
+const table = ref(null)
+
+const route = useRoute()
+const store = useStore()
+watch([route, store.data, store.tables], ([route]) => {
+  entities.value = store.data.value[route.query['t']]
+  table.value = store.tables.value[route.query['t']]
+}, {immediate: true})
+
 </script>
 
 <style lang="scss" scoped>
